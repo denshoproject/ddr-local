f6d385dc88303beaa5f05dc4711a511409dcf55b
geoffrey jost <geoffrey.jost@densho.org>
2013-05-01 13:05:58 -0700

    added an actual test to the @storage_required decorator

--- ddrlocal/webui/views/decorators.py
+++ ddrlocal/webui/views/decorators.py
@@ -1,8 +1,12 @@
 from functools import wraps
 
+from django.conf import settings
+from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.utils.decorators import available_attrs
+
+from DDR import commands
 
 def login_required(func):
     """Checks for git_name,git_main in session; redirects to login if absent
@@ -22,11 +26,18 @@ def storage_required(func):
     """Checks for presence of storage; redirects to mount page if absent
     """
     def storage_present():
-        return True
+        try:
+            repo,org = settings.DDR_ORGANIZATIONS[0].split('-')
+            collections = commands.collections_local(settings.DDR_BASE_PATH, repo, org)
+            return True
+        except:
+            pass
+        return False
     
     @wraps(func, assigned=available_attrs(func))
     def inner(request, *args, **kwargs):
         if not storage_present():
+            messages.error(request, 'ERROR: Could not get list of collections. Is USB HDD plugged in?')
             return HttpResponseRedirect(reverse('webui-storage-required'))
         return func(request, *args, **kwargs)
     return inner
