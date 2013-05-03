e83b2389c3622edd341e50cac00464a21b0113e0
geoffrey jost <geoffrey.jost@densho.org>
2013-05-03 16:33:24 -0700

    clean up the storage mount/unmount page
    Hide some of the stuff that the user doesn't need to see.

--- ddrlocal/storage/forms.py
+++ ddrlocal/storage/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class MountForm(forms.Form):
-    which = forms.CharField(max_length=10, required=True, initial='mount')
+    which = forms.CharField(max_length=10, required=True, initial='mount', widget=forms.HiddenInput)
 
 class UmountForm(forms.Form):
-    which = forms.CharField(max_length=10, required=True, initial='umount')
+    which = forms.CharField(max_length=10, required=True, initial='umount', widget=forms.HiddenInput)
