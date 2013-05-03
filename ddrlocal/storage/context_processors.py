b5cb9484edbfad1747b325a5c0b6b3e20e804481
geoffrey jost <geoffrey.jost@densho.org>
2013-05-03 16:30:53 -0700

    dont need storage mount point in sitewide request context

--- ddrlocal/storage/context_processors.py
+++ ddrlocal/storage/context_processors.py
@@ -8,7 +8,6 @@ from DDR import storage
 def sitewide(request):
     """Variables that need to be inserted into all templates.
     """
-    mountpoint = storage.mount_point(settings.DDR_BASE_PATH)
     stype = storage.storage_type(settings.DDR_BASE_PATH)
     sstatus = storage.storage_status(settings.DDR_BASE_PATH)
     return {
