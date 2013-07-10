80c7ff32a57184e8a460daf883879261a2aa4da9
geoffrey jost <geoffrey.jost@densho.org>
2013-07-09 18:42:55 -0700

    add/rm symlink to ddr dir on USB HDD when mounting/unmounting
    We don't know in advance what the name of the USB HDD will be
    We don't want to store that in a settings file bc we don't want
    to make our users have to edit that file.
    Nginx needs to have a stable file to use for /media/.
    So, symlink to /media/USBHDDNAME/ddr when mounting.

--- ddrlocal/storage/__init__.py
+++ ddrlocal/storage/__init__.py
@@ -13,6 +13,7 @@ REMOUNT_POST_REDIRECT_URL_SESSION_KEY = 'remount_redirect_uri'
 BASE_PATH_TIMEOUT = 60 * 30  # 30 min
 BASE_PATH_DEFAULT = '/tmp/ddr'
 
+
 def base_path(request=None):
     key = 'ddrlocal:base_path'
     path = cache.get(key)
@@ -30,14 +31,55 @@ def base_path(request=None):
             cache.set(key, path, BASE_PATH_TIMEOUT)
     return path
 
+def media_target_dir(base_path):
+    return os.path.join(base_path, settings.DDR_USBHDD_BASE_DIR)
+
+def ddr_media_dir():
+    return '/var/www/ddr/media'
+
+def add_media_symlink(base_path):
+    """Creates symlink to base_path in /var/www/ddr/
+
+    We don't know the USB HDD name in advance, so we can't specify a path
+    to the media directory in the nginx config.  This func adds a symlink
+    from /var/www/ddr/media/ to the ddr/ directory of the USB HDD.
+    """
+    target = base_path
+    link = ddr_media_dir()
+    link_parent = os.path.split(link)[0]
+    s = []
+    if os.path.exists(target):          s.append('1')
+    else:                               s.append('0')
+    if os.path.exists(link_parent):     s.append('1')
+    else:                               s.append('0')
+    if os.access(link_parent, os.W_OK): s.append('1')
+    else:                               s.append('0')
+    s = ''.join(s)
+    if s == '111':
+        os.symlink(target, link)
+
+def rm_media_symlink(base_path):
+    link = ddr_media_dir()
+    s = []
+    if os.path.exists(link):     s.append('1') 
+    else:                        s.append('0')
+    if os.path.islink(link):     s.append('1') 
+    else:                        s.append('0')
+    if os.access(link, os.W_OK): s.append('1') 
+    else:                        s.append('0')
+    if ''.join(s) == '111':
+        os.remove(link)
 
 def mount( request, devicefile, label ):
+    """Mounts requested device, adds /var/www/ddr/media symlink, gives feedback.
+    """
     if not (devicefile and label):
         messages.error(request, 'storage.mount(): devicefile or label missing [{} {}]'.format(devicefile, label))
         return None
     stat,mount_path = commands.mount(devicefile, label)
     if mount_path:
         messages.success(request, 'Mounted {}'.format(label))
+        add_media_symlink(base_path())
         # save label,mount_path in session
         request.session['storage_devicefile'] = devicefile
         request.session['storage_label'] = label
@@ -53,6 +95,7 @@ def mount( request, devicefile, label ):
 def unmount( request, devicefile, label ):
     unmounted = None
     if devicefile:
+        rm_media_symlink(base_path())
         stat,unmounted = commands.umount(devicefile)
         # remove label,mount_path from session,
         # regardless of whether unmount worked
