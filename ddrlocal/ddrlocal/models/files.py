c8d2615e578de8e1db75148f76f0a89414741d95
geoffrey jost <geoffrey.jost@densho.org>
2013-08-20 13:02:21 -0700

    file.links field -> hidden
    We'll un-hide it once we figure out what kind of UI would make
    sense to users.

--- ddrlocal/ddrlocal/models/files.py
+++ ddrlocal/ddrlocal/models/files.py
@@ -155,7 +155,7 @@ FILE_FIELDS = [
             'label':      'Associated Files',
             'help_text':  'Semicolon-separated list of file.path_rels that this file points to.',
             'max_length': 255,
-            'widget':     forms.Textarea,
+            'widget':     forms.HiddenInput,
             'initial':    '',
             'required':   False,
         },
