2115f3d0e0d7fb744a22e61e3bc3d97d129b788b
geoffrey jost <geoffrey.jost@densho.org>
2013-09-03 18:10:55 -0700

    display disk space info for mounted drive in navbar

--- ddrlocal/storage/context_processors.py
+++ ddrlocal/storage/context_processors.py
@@ -5,8 +5,12 @@ from django.conf import settings
 
 from DDR import storage
 
-from storage import base_path
+from storage import base_path, disk_space
 
+BOOTSTRAP_COLORS = {'red': 'btn-danger',
+                    'yellow': 'btn-warning',
+                    'green': 'btn-success',
+                    'unknown': '',}
 
 def sitewide(request):
     """Variables that need to be inserted into all templates.
@@ -14,8 +18,15 @@ def sitewide(request):
     storage_mount_path = base_path(request)
     stype = storage.storage_type(storage_mount_path)
     sstatus = storage.storage_status(storage_mount_path)
+    dspace = disk_space(storage_mount_path)
+    # change color of disk space pill
+    if dspace and dspace.get('percent',None):
+        if    dspace['percent'] <= 10: dspace['label'] = BOOTSTRAP_COLORS['red']
+        elif  dspace['percent'] <= 30: dspace['label'] = BOOTSTRAP_COLORS['yellow']
+        else: dspace['percent']:       dspace['label'] = BOOTSTRAP_COLORS['green']
     return {
         'storage_root': storage_mount_path,
         'storage_type': stype,
         'storage_status': sstatus,
+        'storage_space': dspace,
     }
