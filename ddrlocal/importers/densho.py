cde234d34b94644994d6ac5d1aea207e14f492d3
geoffrey jost <geoffrey.jost@densho.org>
2014-01-07 17:26:58 -0800

    mod script to add importing of ddr-densho files from CSV

--- ddrlocal/importers/densho.py
+++ ddrlocal/importers/densho.py
@@ -1,5 +1,5 @@
 """
-import_densho
+import/densho
 =============
 
 This script will
@@ -9,8 +9,8 @@ This script will
 The Collection must exist on disk for this to work.
 
 
-Walkthrough
------------
+Walkthrough - import entities
+-----------------------------
 
 Log in to VM
 $ cd /usr/local/src/ddr-local/ddrlocal
@@ -43,10 +43,28 @@ $ ./manage.py shell
 
 # Sync with mits
 
+
+Walkthrough - import files
+--------------------------
+
+$ ./manage.py shell
+>>> from importers import densho
+>>> csv_path = '/tmp/ddr-densho-242-files.csv'
+>>> collection_path = '/var/www/media/base/ddr-densho-242'
+>>> git_name = 'gjost'
+>>> git_mail = 'gjost@densho.org'
+>>> densho.import_files(csv_path, collection_path, git_name, git_mail)
+
+# Check everything.
+
+# Sync with mits
+
 """
 
 import csv
 from datetime import datetime
+import logging
+logger = logging.getLogger(__name__)
 import os
 import sys
 
@@ -54,10 +72,20 @@ from django.conf import settings
 
 from DDR import commands
 from webui.models import Collection, Entity
+from ddrlocal.models import DDRLocalFile
 from ddrlocal.models.entity import ENTITY_FIELDS
+from ddrlocal.models.entity import STATUS_CHOICES, PERMISSIONS_CHOICES, RIGHTS_CHOICES
+from ddrlocal.models.entity import LANGUAGE_CHOICES, GENRE_CHOICES, FORMAT_CHOICES
+from ddrlocal.models.files import FILE_FIELDS
+from webui.tasks import add_file
+#def add_file( git_name, git_mail, entity, src_path, role, data ):
+#    print('add_file(%s, %s, %s, %s, %s, %s)' % (git_name, git_mail, entity, src_path, role, data))
+
 
 COLLECTION_FILES_PREFIX = 'files'
-REQUIRED_FIELDS_EXCEPTIONS = ['record_created', 'record_lastmod', 'status', 'rights']
+ENTITY_REQUIRED_FIELDS_EXCEPTIONS = ['record_created', 'record_lastmod',]
+FILE_REQUIRED_FIELDS_EXCEPTIONS = ['thumb',]
+
 
 
 # helper functions -----------------------------------------------------
@@ -74,16 +102,16 @@ def read_csv( path ):
             rows.append(row)
     return rows
 
-def get_required_fields( fields ):
+def get_required_fields( object_class, fields ):
     """Picks out the required fields.
     @param fields: COLLECTION_FIELDS, ENTITY_FIELDS, FILE_FIELDS
     @return list of field names
     """
+    if object_class == 'entity': exceptions = ENTITY_REQUIRED_FIELDS_EXCEPTIONS
+    elif object_class == 'file': exceptions = FILE_REQUIRED_FIELDS_EXCEPTIONS
     required_fields = []
     for field in fields:
-        if field.get('form', None) \
-           and field['form']['required'] \
-           and (field['name'] not in REQUIRED_FIELDS_EXCEPTIONS):
+        if field.get('form', None) and field['form']['required'] and (field['name'] not in exceptions):
             required_fields.append(field['name'])
     return required_fields
 
@@ -116,7 +144,51 @@ def row_missing_required_fields( required_fields, row ):
     missing = [x for x in required_fields if x not in present]
     return missing
 
-def all_rows_valid( headers, required_fields, rows ):
+def valid_choice_values( choices ):
+    """
+    @param choices: List of value:descriptor tuples from MODEL_FIELDS doc.
+    @returns list of values
+    """
+    return [value for value,descriptor in choices]
+
+STATUS_CHOICES_VALUES = valid_choice_values(STATUS_CHOICES)
+PUBLIC_CHOICES_VALUES = valid_choice_values(PERMISSIONS_CHOICES)
+RIGHTS_CHOICES_VALUES = valid_choice_values(RIGHTS_CHOICES)
+LANGUAGE_CHOICES_VALUES = valid_choice_values(LANGUAGE_CHOICES)
+GENRE_CHOICES_VALUES = valid_choice_values(GENRE_CHOICES)
+FORMAT_CHOICES_VALUES = valid_choice_values(FORMAT_CHOICES)
+
+def choice_is_valid( valid_choices, choice ):
+    """Indicates whether choice is valid for CHOICES
+    
+    @param choices: List of valid choice values.
+    @param choice: A particular choice value.
+    @returns True for good, False for bad
+    """
+    if choice in valid_choices:
+        return True
+    return False
+
+def invalid_values( object_class, headers, rowd ):
+    """
+    @param headers: List of field names
+    @param rowd: A single row (dict, not list of fields)
+    @returns False (nothing missing) or a list of fieldnames
+    """
+    invalid = []
+    if object_class == 'entity':
+        if not choice_is_valid(STATUS_CHOICES_VALUES, rowd['status']): invalid.append('status')
+        if not choice_is_valid(PUBLIC_CHOICES_VALUES, rowd['public']): invalid.append('public')
+        if not choice_is_valid(RIGHTS_CHOICES_VALUES, rowd['rights']): invalid.append('rights')
+        if not choice_is_valid(LANGUAGE_CHOICES_VALUES, rowd['language']): invalid.append('language')
+        if not choice_is_valid(GENRE_CHOICES_VALUES, rowd['genre']): invalid.append('genre')
+        if not choice_is_valid(FORMAT_CHOICES_VALUES, rowd['format']): invalid.append('format')
+    elif object_class == 'file':
+        if not choice_is_valid(PUBLIC_CHOICES_VALUES, rowd['public']): invalid.append('public')
+        if not choice_is_valid(RIGHTS_CHOICES_VALUES, rowd['rights']): invalid.append('rights')
+    return invalid
+
+def all_rows_valid( object_class, headers, required_fields, rows ):
     """
     @param headers: List of field names
     @param required_fields: List of required field names
@@ -126,10 +198,20 @@ def all_rows_valid( headers, required_fields, rows ):
     rows_bad = []
     for row in rows:
         rowd = make_row_dict(headers, row)
-        if row_missing_required_fields(required_fields, rowd):
+        missing_required_fields = row_missing_required_fields(required_fields, rowd)
+        invalid = invalid_values(object_class, headers, rowd)
+        if missing_required_fields or invalid:
             rows_bad.append(row)
+        # feedback
+        if missing_required_fields or invalid:
+            print('INVALID ROW')
+            print(row)
+            if missing_required_fields:
+                print('    MISSING REQUIRED FIELDS: %s' % missing_required_fields)
+            if invalid:
+                print('    INVALID VALUES: %s' % invalid)
+            print('')
     return rows_bad
-
 
 
 # import collections ---------------------------------------------------
@@ -140,18 +222,7 @@ def import_collections():
     pass
 
 
-
 # import entities ------------------------------------------------------
-
-def prep_entity_creators( data ): return [x.strip() for x in data.strip().split(';') if x]
-def prep_entity_language( data ):
-    if ':' in data:
-        lang = data.strip().split(':')[0]
-    else:
-        lang = data
-    return lang
-def prep_entity_topics( data ): return [x.strip() for x in data.strip().split(';') if x]
-def prep_entity_persons( data ): return [x.strip() for x in data.strip().split(';') if x]
 
 def import_entities( csv_path, collection_path, git_name, git_mail ):
     """
@@ -163,15 +234,25 @@ def import_entities( csv_path, collection_path, git_name, git_mail ):
     rows = read_csv(csv_path)
     headers = rows[0]
     rows = rows[1:]
-    required_fields = get_required_fields(ENTITY_FIELDS)
-    invalid_rows = all_rows_valid(headers, required_fields, rows)
+    required_fields = get_required_fields('entity', ENTITY_FIELDS)
+    # validate metadata before attempting import
+    invalid_rows = all_rows_valid('entity', headers, required_fields, rows)
     if invalid_rows:
-        print('some rows not valid:')
-        for row in invalid_rows:
-            print(row)
+        print('FILE CONTAINS INVALID ROWS - IMPORT CANCELLED')
     else:
         collection = Collection.from_json(collection_path)
         print(collection)
+        
+        def prep_creators( data ): return [x.strip() for x in data.strip().split(';') if x]
+        def prep_language( data ):
+            if ':' in data:
+                lang = data.strip().split(':')[0]
+            else:
+                lang = data
+            return lang
+        def prep_topics( data ): return [x.strip() for x in data.strip().split(';') if x]
+        def prep_persons( data ): return [x.strip() for x in data.strip().split(';') if x]
+        
         for row in rows:
             rowd = make_row_dict(headers, row)
             
@@ -193,10 +274,10 @@ def import_entities( csv_path, collection_path, git_name, git_mail ):
             # insert values from CSV
             for key in rowd.keys():
                 data = rowd[key]
-                if key == 'creators': data = prep_entity_creators(data)
-                if key == 'language': data = prep_entity_language(data)
-                if key == 'topics': data = prep_entity_topics(data)
-                if key == 'persons': data = prep_entity_persons(data)
+                if key == 'creators': data = prep_creators(data)
+                if key == 'language': data = prep_language(data)
+                if key == 'topics': data = prep_topics(data)
+                if key == 'persons': data = prep_persons(data)
                 setattr(entity, key, data)
             entity.record_created = datetime.now()
             entity.record_lastmod = datetime.now()
@@ -214,7 +295,55 @@ def import_entities( csv_path, collection_path, git_name, git_mail ):
 
 # import files ---------------------------------------------------------
 
-def import_files():
-    """NOT IMPLEMENTED YET
+def import_files( csv_path, collection_path, git_name, git_mail ):
     """
-    pass
+    @param csv_path: Absolute path to CSV data file.
+    @param collection_path: Absolute path to collection repo.
+    @param git_name: Username for use in changelog, git log
+    @param git_mail: User email address for use in changelog, git log
+    """
+    csv_dir = os.path.dirname(csv_path)
+    rows = read_csv(csv_path)
+    headers = rows[0]
+    rows = rows[1:]
+    required_fields = get_required_fields('file', FILE_FIELDS)
+    # validate metadata before attempting import
+    invalid_rows = all_rows_valid('file', headers, required_fields, rows)
+    if invalid_rows:
+        print('FILE CONTAINS INVALID ROWS - IMPORT CANCELLED')
+    else:
+        collection = Collection.from_json(collection_path)
+        print(collection)
+        
+        #def prep_creators( data ): return [x.strip() for x in data.strip().split(';') if x]
+        
+        # check for missing files
+        # if any Entities are missing this will error out
+        missing_files = []
+        for row in rows:
+            rowd = make_row_dict(headers, row)
+            entity_id = rowd.pop('entity_id')
+            repo,org,cid,eid = entity_id.split('-')
+            entity_path = Entity.entity_path(None, repo, org, cid, eid)
+            entity = Entity.from_json(entity_path)
+            src_path = os.path.join(csv_dir, rowd.pop('file'))
+            if not os.path.exists(src_path):
+                missing_files.append(src_path)
+        if missing_files:
+            print('ONE OR MORE SOURCE FILES ARE MISSING! - IMPORT CANCELLED!')
+            for f in missing_files:
+                print('    %s' % f)
+        # files are all accounted for, let's import
+        else:
+            for row in rows:
+                rowd = make_row_dict(headers, row)
+                entity_id = rowd.pop('entity_id')
+                repo,org,cid,eid = entity_id.split('-')
+                entity_path = Entity.entity_path(None, repo, org, cid, eid)
+                entity = Entity.from_json(entity_path)
+                print('entity.id: %s' % entity.id)
+                src_path = os.path.join(csv_dir, rowd.pop('file'))
+                role = rowd.pop('role')
+                print('add_file(%s, %s, %s, %s, %s, %s)' % (git_name, git_mail, entity, src_path, role, rowd))
+                add_file( git_name, git_mail, entity, src_path, role, rowd )
+                print('')
