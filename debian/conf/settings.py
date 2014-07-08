d84df8d8e1f7aed6790248d1151de362914102de
geoffrey jost <geoffrey.jost@densho.org>
2014-07-08 13:02:53 -0700

    new wsgi.py file and settings edit from latest Django version

--- debian/conf/settings.py
+++ debian/conf/settings.py
@@ -1,13 +1,24 @@
-# Django settings for ddrlocal.
+"""
+Django settings for ddrlocal project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# ----------------------------------------------------------------------
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-# ----------------------------------------------------------------------
-
 import ConfigParser
 import logging
-import os
 
 os.environ['USER'] = 'ddr'
 
@@ -124,13 +135,13 @@ MANAGERS = ADMINS
 SITE_ID = 1
 
 INSTALLED_APPS = (
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.admin',
     #
     'bootstrap_pagination',
     'djcelery',
@@ -308,8 +319,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
