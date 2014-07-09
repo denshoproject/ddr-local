3ee1af5fe1797cd43b94dec5098e2f354408e7a0
geoffrey jost <geoffrey.jost@densho.org>
2014-07-08 18:31:36 -0700

    rm superfluous call to collection.repo_fetch
    For some reason this often uses the wrong path but also is
    unnecessary because repo_behind uses cached gitstatus data.

--- ddrlocal/webui/views/files.py
+++ ddrlocal/webui/views/files.py
@@ -146,7 +146,6 @@ def new( request, repo, org, cid, eid, role='master' ):
     if collection.locked():
         messages.error(request, WEBUI_MESSAGES['VIEWS_COLL_LOCKED'].format(collection.id))
         return HttpResponseRedirect( reverse('webui-entity', args=[repo,org,cid,eid]) )
-    collection.repo_fetch()
     if collection.repo_behind():
         messages.error(request, WEBUI_MESSAGES['VIEWS_COLL_BEHIND'].format(collection.id))
         return HttpResponseRedirect( reverse('webui-entity', args=[repo,org,cid,eid]) )
