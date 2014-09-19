0ba4d080e5f88f4db8527c7b7e59ea2b70858dbd
geoffrey jost <geoffrey.jost@densho.org>
2014-09-19 12:56:04 -0700

    tests for models functions (labels_vals,load/dump,formprep/post)

---
+++ ddrlocal/ddrlocal/models/test.py
@@ -0,0 +1,84 @@
+from datetime import datetime
+
+
+MODEL = 'test'
+
+FIELDS = [
+    {
+        'name':       'id',
+        'model_type': str,
+        'default':    '',
+        'inheritable': '',
+        'form_type':  '',
+        'form': {
+            'label':     'ID',
+            'help_text': '',
+            'widget':    '',
+            'initial':   '',
+        },
+        'xpath':      "",
+        'xpath_dup':  [],
+    },
+    {
+        'name':       'timestamp',
+        'model_type': datetime,
+        'default':    '',
+        'inheritable': '',
+        'form_type':  '',
+        'form': {
+            'label':     'Timestamp',
+            'help_text': '',
+            'widget':    '',
+            'initial':   '',
+        },
+        'xpath':      "",
+        'xpath_dup':  [],
+    },
+    {
+        'name':       'status',
+        'model_type': int,
+        'default':    '',
+        'inheritable': '',
+        'form_type':  '',
+        'form': {
+            'label':     'Status',
+            'help_text': '',
+            'widget':    '',
+            'initial':   '',
+        },
+        'xpath':      "",
+        'xpath_dup':  [],
+    },
+    {
+        'name':       'title',
+        'model_type': str,
+        'default':    '',
+        'inheritable': '',
+        'form_type':  '',
+        'form': {
+            'label':     'Title',
+            'help_text': '',
+            'widget':    '',
+            'initial':   '',
+        },
+        'xpath':      "",
+        'xpath_dup':  [],
+    },
+    {
+        'name':       'description',
+        'model_type': str,
+        'default':    '',
+        'inheritable': '',
+        'form_type':  '',
+        'form': {
+            'label':     'Description',
+            'help_text': '',
+            'widget':    '',
+            'initial':   '',
+        },
+        'xpath':      "",
+        'xpath_dup':  [],
+    },
+]
+
+UNDEFINED = True
