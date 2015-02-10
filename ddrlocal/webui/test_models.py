1878ee1adddbaa3cf7fc11f5a2e2d6fa507ffcfd
geoffrey jost <geoffrey.jost@densho.org>
2015-02-09 18:39:59 -0800

    mv ddrlocal.models tests to webui; update test placeholders

--- ddrlocal/webui/test_models.py
+++ ddrlocal/webui/test_models.py
@@ -1,4 +1,28 @@
-from ddrlocal import models
+import json
+
+from webui import models
+
+
+TEST_DOCUMENT = """[
+    {
+        "application": "https://github.com/densho/ddr-local.git",
+        "commit": "52155f819ccfccf72f80a11e1cc53d006888e283  (HEAD, repo-models) 2014-09-16 16:30:42 -0700",
+        "git": "git version 1.7.10.4; git-annex version: 3.20120629",
+        "models": "",
+        "release": "0.10"
+    },
+    {"id": "ddr-test-123"},
+    {"timestamp": "2014-09-19T03:14:59"},
+    {"status": 1},
+    {"title": "TITLE"},
+    {"description": "DESCRIPTION"}
+]"""
+
+class Document(object):
+    pass
+
+class Form(object):
+    pass
 
 
 def test_git_version():
@@ -7,101 +31,94 @@ def test_git_version():
     assert 'git-annex version' in out
     assert 'local repository version' in out
 
-# TODO module_function
-# TODO module_xml_function
-# TODO write_json
-# TODO _inheritable_fields
-# TODO _inherit
-# TODO hash
+# TODO repo_models_valid
+# TODO model_def_commits
+# TODO model_def_fields
 
-# TODO DDRLocalCollection.__init__
-# TODO DDRLocalCollection.__repr__
-# TODO DDRLocalCollection.url
-# TODO DDRLocalCollection.cgit_url
-# TODO DDRLocalCollection.collection_path
-# TODO DDRLocalCollection.repo_fetch
-# TODO DDRLocalCollection.repo_status
-# TODO DDRLocalCollection.repo_synced
-# TODO DDRLocalCollection.repo_ahead
-# TODO DDRLocalCollection.repo_behind
-# TODO DDRLocalCollection.repo_diverged
-# TODO DDRLocalCollection.repo_conflicted
-# TODO DDRLocalCollection.repo_annex_status
-# TODO DDRLocalCollection._lockfile
-# TODO DDRLocalCollection.lock
-# TODO DDRLocalCollection.unlock
-# TODO DDRLocalCollection.locked
-# TODO DDRLocalCollection.create
-# TODO DDRLocalCollection.entities
-# TODO DDRLocalCollection.inheritable_fields
-# TODO DDRLocalCollection.labels_values
-# TODO DDRLocalCollection.form_prep
-# TODO DDRLocalCollection.form_post
-# TODO DDRLocalCollection.json
-# TODO DDRLocalCollection.from_json
-# TODO DDRLocalCollection.load_json
-# TODO DDRLocalCollection.dump_json
-# TODO DDRLocalCollection.write_json
-# TODO DDRLocalCollection.ead
-# TODO DDRLocalCollection.dump_ead
+def test_form_prep():
+    document = Document()
+    models.load_json(document, testmodule, TEST_DOCUMENT)
+    expected = {
+        'id': u'ddr-test-123',
+        'timestamp': u'2014-09-19T03:14:59',
+        'status': 1,
+        'title': u'TITLE',
+        'description': u'DESCRIPTION',
+    }
+    assert models.form_prep(document, testmodule) == expected
 
-# TODO DDRLocalEntity.__init__
-# TODO DDRLocalEntity.__repr__
-# TODO DDRLocalEntity.url
-# TODO DDRLocalEntity.entity_path
-# TODO DDRLocalEntity._lockfile
-# TODO DDRLocalEntity.lock
-# TODO DDRLocalEntity.unlock
-# TODO DDRLocalEntity.locked
-# TODO DDRLocalEntity.files_master
-# TODO DDRLocalEntity.files_mezzanine
-# TODO DDRLocalEntity.detect_file_duplicates
-# TODO DDRLocalEntity.rm_file_duplicates
-# TODO DDRLocalEntity.file
-# TODO DDRLocalEntity.files_log
-# TODO DDRLocalEntity.create
-# TODO DDRLocalEntity.inherit
-# TODO DDRLocalEntity.inheritable_fields
-# TODO DDRLocalEntity.labels_values
-# TODO DDRLocalEntity.form_prep
-# TODO DDRLocalEntity.form_post
-# TODO DDRLocalEntity.json
-# TODO DDRLocalEntity.from_json
-# TODO DDRLocalEntity._load_file_objects
-# TODO DDRLocalEntity.load_json
-# TODO DDRLocalEntity.dump_json
-# TODO DDRLocalEntity.write_json
-# TODO DDRLocalEntity.mets
-# TODO DDRLocalEntity.dump_mets
-# TODO DDRLocalEntity.add_file
-# TODO DDRLocalEntity.add_access
+def test_form_post():
+    document = Document()
+    form = Form()
+    form.cleaned_data = {
+        'id': 'ddr-test-123',
+        'timestamp': '2014-09-19T12:34:56',
+        'status': 0,
+        'title': 'NEW TITLE',
+        'description': 'NEW DESCRIPTION',
+    }
+    models.load_json(document, testmodule, TEST_DOCUMENT)
+    models.form_post(document, testmodule, form)
+    assert document.id == "ddr-test-123"
+    assert document.timestamp == "2014-09-19T12:34:56"
+    assert document.status == 0
+    assert document.title == "NEW TITLE"
+    assert document.description == "NEW DESCRIPTION"    
 
-# TODO DDRLocalFile.__init__
-# TODO DDRLocalFile.__repr__
-# TODO DDRLocalFile.url
-# TODO DDRLocalFile.media_url
-# TODO DDRLocalFile.access_url
-# TODO DDRLocalFile.file_path
-# TODO DDRLocalFile.files_rel
-# TODO DDRLocalFile.present
-# TODO DDRLocalFile.access_present
-# TODO DDRLocalFile.inherit
-# TODO DDRLocalFile.labels_values
-# TODO DDRLocalFile.form_prep
-# TODO DDRLocalFile.form_post
-# TODO DDRLocalFile.from_json
-# TODO DDRLocalFile.load_json
-# TODO DDRLocalFile.dump_json
-# TODO DDRLocalFile.write_json
-# TODO DDRLocalFile.file_name
-# TODO DDRLocalFile.set_path
-# TODO DDRLocalFile.set_access
-# TODO DDRLocalFile.file
-# TODO DDRLocalFile.dict
-# TODO DDRLocalFile.extract_xmp
-# TODO DDRLocalFile.access_file_name
-# TODO DDRLocalFile.make_access_file
-# TODO DDRLocalFile.make_thumbnail
-# TODO DDRLocalFile.links_incoming
-# TODO DDRLocalFile.links_outgoing
-# TODO DDRLocalFile.links_all
+# TODO post_json
+# TODO _child_jsons
+# TODO _selected_inheritables
+# TODO _selected_field_values
+# TODO _load_object
+# TODO _update_inheritables
+
+# TODO Collection.__repr__
+# TODO Collection.collection_path
+# TODO Collection.gitstatus_path
+# TODO Collection.url
+# TODO Collection.cgit_url
+# TODO Collection.cache_delete
+# TODO Collection.from_json
+# TODO Collection.repo_fetch
+# TODO Collection.repo_status
+# TODO Collection.repo_annex_status
+# TODO Collection._repo_state
+# TODO Collection.repo_synced
+# TODO Collection.repo_ahead
+# TODO Collection.repo_behind
+# TODO Collection.repo_diverged
+# TODO Collection.repo_conflicted
+# TODO Collection.sync_status
+# TODO Collection.sync_status_url
+# TODO Collection.gitstatus
+# TODO Collection.selected_inheritables
+# TODO Collection.update_inheritables
+# TODO Collection.model_def_commits
+# TODO Collection.model_def_fields
+# TODO Collection.form_prep
+# TODO Collection.form_post
+# TODO Collection.post_json
+
+# TODO Entity.__repr__
+# TODO Entity.entity_path
+# TODO Entity.url
+# TODO Entity.from_json
+# TODO Entity.selected_inheritables
+# TODO Entity.update_inheritables
+# TODO Entity.model_def_commits
+# TODO Entity.model_def_fields
+# TODO Entity.form_prep
+# TODO Entity.form_post
+# TODO Entity.post_json
+# TODO Entity.load_file_objects
+
+# TODO DDRFile.__repr__
+# TODO DDRFile.url
+# TODO DDRFile.media_url
+# TODO DDRFile.access_url
+# TODO DDRFile.file_path
+# TODO DDRFile.model_def_commits
+# TODO DDRFile.model_def_fields
+# TODO DDRFile.form_prep
+# TODO DDRFile.form_post
+# TODO DDRFile.post_json
