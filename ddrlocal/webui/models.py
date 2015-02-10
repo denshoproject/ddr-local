7c16cd5620dfa85ea60538401bb4b74d85c65656
geoffrey jost <geoffrey.jost@densho.org>
2015-02-09 20:01:40 -0800

    object attribute inheritance code from webui.models to DDR.models

--- ddrlocal/webui/models.py
+++ ddrlocal/webui/models.py
@@ -162,101 +162,6 @@ def post_json(hosts, index, json_path):
 
 # functions relating to inheritance ------------------------------------
 
-def _child_jsons( path ):
-    """List all the .json files under path directory; excludes specified dir.
-    
-    @param path: Absolute directory path.
-    @return list of paths
-    """
-    paths = []
-    r = envoy.run('find %s -name "*.json" ! -name ".git" -print' % path)
-    if not r.status_code:
-        for p in r.std_out.strip().split('\n'):
-            if os.path.dirname(p) != path:
-                paths.append(p)
-    return paths
-
-def _selected_inheritables( inheritables, cleaned_data ):
-    """Indicates which inheritable fields from the list were selected in the form.
-    
-    Selector fields are assumed to be BooleanFields named "FIELD_inherit".
-    
-    @param inheritables: List of field/attribute names.
-    @param cleaned_data: form.cleaned_data.
-    @return
-    """
-    fieldnames = {}
-    for field in inheritables:
-        fieldnames['%s_inherit' % field] = field
-    selected = []
-    if fieldnames:
-        for key in cleaned_data.keys():
-            if (key in fieldnames.keys()) and cleaned_data[key]:
-                selected.append(fieldnames[key])
-    return selected
-
-def _selected_field_values( parent_object, inheritables ):
-    """Gets list of selected inherited fieldnames and their values from the parent object
-    
-    @param parent_object
-    @param inheritables
-    """
-    field_values = []
-    for field in inheritables:
-        value = getattr(parent_object, field)
-        field_values.append( (field,value) )
-    return field_values
-
-def _load_object( json_path ):
-    """Loads File, Entity, or Collection from JSON file
-    
-    @param json_path
-    """
-    dirname = os.path.dirname(json_path)
-    basename = os.path.basename(json_path)
-    if ('master' in basename) or ('mezzanine' in basename):  # file
-        entity = Entity.from_json(os.path.dirname(dirname))
-        fname = os.path.splitext(basename)[0]
-        repo,org,cid,eid,role,sha1 = fname.split('-')
-        return entity.file(repo, org, cid, eid, role, sha1)
-    elif basename == 'entity.json':
-        return Entity.from_json(dirname)
-    elif basename == 'collection.json':
-        return Collection.from_json(dirname)
-    return None
-    
-def _update_inheritables( parent_object, objecttype, inheritables, cleaned_data ):
-    """Update specified inheritable fields of child objects using form data.
-    
-    @param parent_object: A Collection, Entity, or File
-    @param cleaned_data: Form cleaned_data from POST.
-    @returns: tuple containing list of changed object Ids and list of changed objects' JSON files.
-    """
-    child_ids = []
-    changed_files = []
-    # values of selected inheritable fields from parent
-    field_values = _selected_field_values(parent_object, inheritables)
-    # load child objects and apply the change
-    if field_values:
-        for child_json in _child_jsons(parent_object.path):
-            child = _load_object(child_json)
-            if child:
-                # set field if exists in child and doesn't already match parent value
-                changed = False
-                for field,value in field_values:
-                    if hasattr(child, field):
-                        existing_value = getattr(child,field)
-                        if existing_value != value:
-                            setattr(child, field, value)
-                            changed = True
-                # write json and add to list of changed IDs/files
-                if changed:
-                    child.write_json()
-                    if hasattr(child, 'id'):         child_ids.append(child.id)
-                    elif hasattr(child, 'basename'): child_ids.append(child.basename)
-                    changed_files.append(child_json)
-    return child_ids,changed_files
-
 
 
 class Collection( DDRCollection ):
@@ -377,12 +282,6 @@ class Collection( DDRCollection ):
     
     def gitstatus( self, force=False ):
         return gitstatus.read(settings.MEDIA_BASE, self.path)
-
-    def selected_inheritables(self, cleaned_data ):
-        return _selected_inheritables(self.inheritable_fields(), cleaned_data)
-    
-    def update_inheritables( self, inheritables, cleaned_data ):
-        return _update_inheritables(self, 'collection', inheritables, cleaned_data)
     
     def model_def_commits(self):
         """Assesses document's relation to model defs in 'ddr' repo.
@@ -444,12 +343,6 @@ class Entity( DDREntity ):
     @staticmethod
     def from_json(entity_abs):
         return from_json(Entity, os.path.join(entity_abs, 'entity.json'))
-    
-    def selected_inheritables(self, cleaned_data ):
-        return _selected_inheritables(self.inheritable_fields(), cleaned_data)
-    
-    def update_inheritables( self, inheritables, cleaned_data ):
-        return _update_inheritables(self, 'entity', inheritables, cleaned_data)
     
     def model_def_commits(self):
         """Assesses document's relation to model defs in 'ddr' repo.
