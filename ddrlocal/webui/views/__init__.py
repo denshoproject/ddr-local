aaf8b1b1e2c2fa31e1db4ff4ba046a912ada3042
geoffrey jost <geoffrey@jostwebwerks.com>
2016-06-03 16:52:29 -0700

    login: add IDSERVICE_API_BASE to login error messages
    Lets you know that you've got the wrong URL in your configs.

--- ddrlocal/webui/views/__init__.py
+++ ddrlocal/webui/views/__init__.py
@@ -45,19 +45,25 @@ def login( request ):
             if status1 != 200:
                 messages.warning(
                     request,
-                    'Login failed[1]: %s %s' % (status1,reason1)
+                    'Login failed: %s %s (%s) [1]' % (
+                        status1,reason1,settings.IDSERVICE_API_BASE
+                    )
                 )
                 return HttpResponseRedirect(redirect_uri)
             if status2 != 200:
                 messages.warning(
                     request,
-                    'Login failed[2]: %s %s' % (status2,reason2)
+                    'Login failed: %s %s (%s) [2]' % (
+                        status2,reason2, settings.IDSERVICE_API_BASE
+                    )
                 )
                 return HttpResponseRedirect(redirect_uri)
             if not (userinfo['email'] and userinfo['first_name'] and userinfo['last_name']):
                 messages.warning(
                     request,
-                    'Login failed[3]: ID service missing required user info (email, first_name, last_name).'
+                    'Login failed: ID service missing required user info (email, first_name, last_name). (%s) [3]' % (
+                        settings.IDSERVICE_API_BASE
+                    )
                 )
                 return HttpResponseRedirect(redirect_uri)
 
