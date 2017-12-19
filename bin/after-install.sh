855f182e7081fdc6271f6a655551a801937c27a5
geoffrey jost <geoffrey@jostwebwerks.com>
2017-12-19 12:49:58 -0800

    deb post-install: create and set perms for thumbnails dir

--- bin/after-install.sh
+++ bin/after-install.sh
@@ -22,15 +22,20 @@ then
     rm /etc/nginx/sites-enabled/default
 fi
 
-# logs dir perms
+# logs dir
 mkdir -p /var/log/ddr
 chmod 755 /var/log/ddr
 chown -R ddr.ddr /var/log/ddr
 
-# sqlite3 database dir perms
+# sqlite3 database dir
 mkdir -p /var/lib/ddr
 chmod 755 /var/lib/ddr
 chown -R ddr.ddr /var/lib/ddr
+
+# thumbnails dir
+mkdir -p /var/www/media/cache
+chmod 755 /var/www/media/cache
+chown -R ddr.ddr /var/www/media/cache
 
 # static dir symlinks
 
