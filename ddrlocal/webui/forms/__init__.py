0ba29ac842e796b2e7b415f9a5b680d0ca75be35
geoffrey jost <geoffrey@jostwebwerks.com>
2018-09-28 17:23:30 -0700

    forms: Made Object form with with new bottom-up inheritance
    Previously, ddr-defs parent object fields had 'inheritable'
    attributes, and all child objects with that field could inherit
    parent objects' field value.
    
    Now, child object fields have 'inherits' attributes which specify
    which parent fields they inherit from.  This makes it possible
    for children to inherit values where the field names don't match.
    
    Example: entity.credit inherits from collection.prefercite.

--- ddrlocal/webui/forms/__init__.py
+++ ddrlocal/webui/forms/__init__.py
@@ -10,7 +10,7 @@ from django.conf import settings
 from django.utils.encoding import force_text
 
 from DDR import modules
-from webui.identifier import Identifier
+from webui.identifier import Identifier, INHERITABLE_FIELDS
 from ..util import OrderedDict
 
 INTERVIEW_SIG_PATTERN = r'^denshovh-[a-z_0-9]{1,}-[0-9]{2,2}$'
@@ -236,8 +236,9 @@ def construct_form(model_fields):
                 form_field_object = getattr(forms, field_name)
                 fobject = form_field_object(*[], **fkwargs['form'])
                 fields.append((fkwargs['name'], fobject))
-        # if field is inheritable, add inherit-this checkbox
-        if fkwargs.get('inheritable', None):
+        # if field has children that inherit from it, add inherit-this checkbox
+        model_field = '.'.join([fkwargs['model'], fkwargs['name']])
+        if model_field in INHERITABLE_FIELDS:
             helptext = "Apply value of %s to this object's children" % fkwargs['form']['label']
             #CHOICES = ((1, helptext),)
             #fobject = forms.ChoiceField(label='', choices=CHOICES,
