df8486b3594ac67e0c918bf7f5c93ce90d35662c
Geoffrey Jost <geoffrey@jostwebwerks.com>
2020-03-25 10:29:46 -0700

    urls: String references to import app urls

--- ddrlocal/ddrlocal/urls.py
+++ ddrlocal/ddrlocal/urls.py
@@ -2,11 +2,6 @@ from django.conf.urls import include, url
 from django.views.debug import technical_500_response
 from django.views.generic import TemplateView
 
-from djcelery import urls as djcelery_urls
-
-from storage import urls as storage_urls
-from webui import urls as webui_urls
-
 
 DEBUG_TEXT = """
 Scroll down to view request metadata and application settings.
@@ -25,8 +20,8 @@ urlpatterns = [
     url(r'^404/', TemplateView.as_view(template_name="ddrlocal/404.html")),
     url(r'^500/', TemplateView.as_view(template_name="ddrlocal/500.html")),
     url(r'^debug/', debug, name='debug'),
-    url(r'^celery/', include(djcelery_urls)),
-    url(r'^storage/', include(storage_urls)),
-    url(r'^ui/', include(webui_urls)),
+    url(r'^celery/', include('djcelery.urls')),
+    url(r'^storage/', include('storage.urls')),
+    url(r'^ui/', include('webui.urls')),
     url(r'^$', TemplateView.as_view(template_name="webui/index.html"), name='index'),
 ]
